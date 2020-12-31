91d47cbe23933123e6eb5f39029d2244484847ce
April Darger <april.m.darger@gmail.com>
2020-12-31 16:48:06 -0700

    Routing.

--- api/server.js
+++ api/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const authRouter = require('./auth/router');
+const usersRouter = require('./users/router');
+const issuesRouter = require('./issues/router');
+
 const server = express();
 
+server.use(helmet());
+server.use(morgan());
 server.use(express.json());
+server.use(cors());
+
+server.use('/api/issues', issuesRouter);
+server.use('/api/users', usersRouter);
+server.use('/api', authRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: 'up' });
