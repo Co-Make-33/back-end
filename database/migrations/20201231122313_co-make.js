5785001f0a8cd5a63cfb80a26b2db2b458e8c39a
April Darger <april.m.darger@gmail.com>
2020-12-31 21:46:50 -0700

    Added vote table.

--- database/migrations/20201231122313_co-make.js
+++ database/migrations/20201231122313_co-make.js
@@ -21,13 +21,29 @@ exports.up = function(knex) {
     .createTable('comments', table => {
       table.increments('comment_id')
       table.string('comment').notNullable()
-      table.string('user_id').notNullable()
       table.timestamp('comment_date').defaultTo(knex.fn.now())
+      table.integer('user_id')
+        .unsigned()
+        .references('user_id').inTable('users')
+        .onDelete('RESTRICT').onUpdate('RESTRICT')
+    })
+    .createTable('votes', table => {
+      table.increments('vote_id')
+      table.integer('user_id')
+        .unsigned()
+        .references('user_id').inTable('users')
+        .onDelete('RESTRICT').onUpdate('RESTRICT')
+      table.integer('issue_id')
+        .unsigned()
+        .references('issue_id').inTable('issues')
+        .onDelete('RESTRICT').onUpdate('RESTRICT')
+      table.integer('vote')
     });
 };
 
 exports.down = function(knex) {
   return knex.schema
+    .dropTableIfExists('votes')
     .dropTableIfExists('comments')
     .dropTableIfExists('issues')
     .dropTableIfExists('users');
