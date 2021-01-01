c4621d2848a42f68c38e05cadb9449abc8d49c34
April Darger <april.m.darger@gmail.com>
2020-12-31 23:25:49 -0700

    Changed id names for tables.

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
   return knex('users').truncate()
     .then(function () {
       return knex('users').insert([
-        {user_id: 1, username: 'Paco', email: 'p@p.com', password: '123123', },
-        {user_id: 2, username: 'Nate', email: 'n@n.com', password: '123123', },
-        {user_id: 3, username: 'NoName', email: 'no@no.com', password: '123123', }
+        {id: 1, username: 'Paco', email: 'p@p.com', password: '123123', },
+        {id: 2, username: 'Nate', email: 'n@n.com', password: '123123', },
+        {id: 3, username: 'NoName', email: 'no@no.com', password: '123123', }
       ]);
     });
 };
--- database/seeds/002-issues.js
+++ database/seeds/002-issues.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
   return knex('issues').truncate()
     .then(function () {
       return knex('issues').insert([
-        {issue_id: 1, title: 'potholes', description: 'All the freaking holes in the roads! Why does this need an explanation?', resolved_status: 'unresolved', user_id: 1},
-        {issue_id: 2, title: 'racoons', description: 'We all need trash pandas in our lives. Import racoons!!!', resolved_status: 'unresolved', user_id: 3},
-        {issue_id: 3, title: 'feral trees', description: 'The trees are infected with albienuctious and attacking sweet little kitties!', resolved_status: 'unresolved', user_id: 2}
+        {id: 1, title: 'potholes', description: 'All the freaking holes in the roads! Why does this need an explanation?', resolved_status: 'unresolved', user_id: 1},
+        {id: 2, title: 'racoons', description: 'We all need trash pandas in our lives. Import racoons!!!', resolved_status: 'unresolved', user_id: 3},
+        {id: 3, title: 'feral trees', description: 'The trees are infected with albienuctious and attacking sweet little kitties!', resolved_status: 'unresolved', user_id: 2}
       ]);
     });
 };
