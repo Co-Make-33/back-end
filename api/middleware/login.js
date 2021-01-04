4482fd676b7a83ea8f9c248f150da0009d20eeee
April Darger <april.m.darger@gmail.com>
2021-01-03 23:58:30 -0700

    Auth and restricted middleware.

---
+++ api/middleware/login.js
@@ -0,0 +1,69 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const Users = require('../users/model');
+const { jwtSecret } = require('../../config/secrets');
+
+
+const logBody = (req, res, next) => {
+  const email = req.body.email;
+  const username = req.body.username;
+  if((!!username && !!email) || (!username && !email)) {
+    res.status(401).json({ message: 'Email OR Username required' });
+  } else if (!req.body.password) {
+    res.status(401).json({ message: 'Password required' });
+  } else {
+    next();
+  }
+}
+
+const validUser = async (req, res, next) => {
+  const email = req.body.email;
+  const username = req.body.username;
+  try {
+    const rows = await Users.getBy(email ? { email } : { username });
+    if (!rows.length) {
+      res.status(401).json({ message: 'invalid credentials' });
+    } else {
+      req.userData = rows[0];
+      next();
+    }
+  } catch (error) {
+    res.status(500).json({ message:error.message });
+  }
+}
+
+const validPass = (req, res, next) => {
+  try {
+    console.log('Password UserData', req.userData.password);
+    console.log('Password Body', req.body.password);
+    
+    const verify = bcrypt.compareSync(req.body.password, req.userData.password);
+    
+    if (verify) {
+      next();
+    } else {
+      res.status(401).json({ message: 'invalid credentials' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+const makeToken = (user) => {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: '1h',
+  };
+  return jwt.sign(payload, jwtSecret, options);
+};
+
+module.exports = {
+  logBody,
+  validUser,
+  validPass,
+  makeToken
+}
---
+++ api/middleware/register.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs');
+const Users = require('../users/model');
+
+const regBody = (req, res, next) => {
+  if (!req.body.password || !req.body.username || !req.body.email) {
+     res.status(401).json({ message: 'username, email, and password required' });
+  } else {
+    next();
+  }
+};
+
+const unique = async (req, res, next) => {
+  try {
+    const rowsUsername = await Users.getBy({ username: req.body.username });
+    const rowsEmail = await Users.getBy({ email: req.body.email });    
+    if (!rowsUsername.length || !rowsEmail.length){
+      next()
+    } else {
+      res.status(401).json({message: 'Username or email already in use'})
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+const hashPass = (req, res) => {
+  const rounds = process.env.BCRYPT_ROUNDS || 7;
+  const hash = bcrypt.hashSync(req.body.password, parseInt(rounds));
+  req.body.password = hash;
+};
+
+module.exports = {
+  regBody,
+  hashPass,
+  unique
+}
