4302d90705263c18a37ef35a6090736807820b01
April Darger <april.m.darger@gmail.com>
2021-01-03 23:59:53 -0700

    auth endpoints, restricted routing.

--- api/auth/router.js
+++ api/auth/router.js
@@ -1,12 +1,22 @@
 const router = require('express').Router();
-const bcryptjs = require('bcryptjs');
 
-router.post('/register', (req, res) => {
+const { regBody, hashPass, unique } = require ('../middleware/register.js');
+const { logBody, validUser, validPass, makeToken } = require('../middleware/login');
 
+const Users = require('../users/model');
+
+router.post('/register', regBody, unique, hashPass, async (req, res) => {
+  try {
+    const newUser = await Users.add(req.body);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
-router.post('/login', (req, res) => {
-
+router.post('/login',logBody, validUser, validPass, (req, res) => {
+  const token = makeToken(req.userData);
+  res.status(200).json({ message:`Welcome, ${req.userData.username}`, token });
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const restrict = require('./middleware/restricted');
+
 const authRouter = require('./auth/router');
 const usersRouter = require('./users/router');
 const issuesRouter = require('./issues/router');
@@ -10,12 +12,12 @@ const issuesRouter = require('./issues/router');
 const server = express();
 
 server.use(helmet());
-server.use(morgan());
+server.use(morgan('dev'));
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/issues', issuesRouter);
-server.use('/api/users', usersRouter);
+server.use('/api/issues', restrict, issuesRouter);
+server.use('/api/users', restrict, usersRouter);
 server.use('/api', authRouter);
 
 server.get('/', (req, res) => {
