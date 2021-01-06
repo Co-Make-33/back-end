db5fd5e2558d1487ce7392d238eb72ec41fe3dd4
April Darger <april.m.darger@gmail.com>
2021-01-06 15:27:21 -0700

    Updated votes seeds to reflect upvote/downvote.

--- database/seeds/004-votes.js
+++ database/seeds/004-votes.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
   return knex('votes').del()
     .then(function () {
       return knex('votes').insert([
-        {id: 1, user_id: 1, issue_id: 1},
-        {id: 2, user_id: 3, issue_id: 2},
-        {id: 3, user_id: 1, issue_id: 2}
+        {id: 1, user_id: 1, issue_id: 1, upvote: 1, downvote: null},
+        {id: 2, user_id: 3, issue_id: 2, upvote: 1, downvote: null},
+        {id: 3, user_id: 1, issue_id: 2, upvote: 1, downvote: null}
       ]);
     });
 };
