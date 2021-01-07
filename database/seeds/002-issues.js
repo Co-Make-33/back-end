ff27410ca23f1712e0d959b22adb7e6998a1171e
April Darger <april.m.darger@gmail.com>
2021-01-07 11:26:03 -0700

    Changing .truncate back to .del because Heroku is not loading. Build was successful, but open app just spins and never gets 'up'.

--- database/seeds/002-issues.js
+++ database/seeds/002-issues.js
@@ -1,6 +1,6 @@
 
 exports.seed = function(knex) {
-  return knex('issues').truncate()
+  return knex('issues').del()
     .then(function () {
       return knex('issues').insert([
         {id: 1, title: 'potholes', description: 'All the freaking holes in the roads! Why does this need an explanation?', resolved_status: 'unresolved', user_id: 1},
